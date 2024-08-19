62174872314bbc6bb20a67ff787c5dff2c38824e
Matilde Campanardi <139547590+matildecmp@users.noreply.github.com>
2024-08-19 21:10:47 +0200

    Update articoli.js

--- articoli.js
+++ articoli.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
     $('.expan, .ex, .corr, .reg').hide(); /* Interventi editoriali inizialmente nascosti */
 
-    /* Gestione pulsanti categorie + colorazione testo */
+    /* Gestione pulsanti categorie + colorazione testo + pulsante Ripulisci */
     $('.categ').click(function () {
         $(this).toggleClass('active');
         $btnId = $(this).attr('id');
@@ -10,15 +10,19 @@ $(document).ready(function () {
         if ($(this).hasClass('active')) {
             $(this).css('box-shadow', '0.5px 0.5px 10px red');
             $(this).css('transition', '0.3s ease');
-            $('span.' + $btnId).css('background-color', $color);
+            $('span.' + $btnId).css('background', $color);
+            $("button#clean").click(function () {
+                $('button').css('box-shadow', '0.5px 0.5px 2px grey');
+                $('span.person, span.character, span.bibl, span.place, span.event, span.org, span.num, span.date, span.verbum, span.cit, span.foreign').css('background', 'transparent');
+            });
         } else {
             $(this).css('box-shadow', '0.5px 0.5px 2px grey');
             $(this).css('transition', '0.3s ease');
-            $('span.' + $btnId).css('background-color', 'transparent');
+            $('span.' + $btnId).css('background', 'transparent');
         }
     });
 
-    /* Gestione pulsanti interventi + colorazione testo */
+    /* Gestione pulsanti interventi + colorazione testo + pulsante Ripulisci */
     $('.int').click(function () { 
         $(this).toggleClass('active');
         $btnId = $(this).attr('id');
@@ -27,7 +31,11 @@ $(document).ready(function () {
             $(this).css('box-shadow', '0.5px 0.5px 10px red');
             $(this).css('transition', '0.3s ease');
             $('span.' + $btnId).show();
-            $('span.' + $btnId).css('background-color', $color);
+            $('span.' + $btnId).css('background', $color);
+            $("button#clean").click(function () {
+                $('button').css('box-shadow', '0.5px 0.5px 2px grey');
+                $('span.ex, span.expan, span.corr, span.reg').hide();
+            });
         } else {
             $(this).css('box-shadow', '0.5px 0.5px 2px grey');
             $(this).css('transition', '0.3s ease');
@@ -51,5 +59,27 @@ $(document).ready(function () {
     $("span.cit").mouseout(function(){ 
         $(this).next("span.info").css("visibility","hidden");
     });
+
+    /* Gestione area interattiva facsimile */
+    var previousSpan = null;
+    
+    $("area").click(function() {
+        var areaId = $(this).attr("id");
+        var dataCorr = $(this).attr("data-corresp");
+        if (dataCorr.startsWith("#")) {
+            var spanId = dataCorr.substring(1);
+            var spanCorr = $("#" + spanId);
+            if (previousSpan && previousSpan.length > 0) {
+                previousSpan.removeClass('selected');
+            }
+            if (spanCorr.length > 0) {
+                spanCorr.addClass('selected');
+                $('html, body').animate({
+                    scrollTop: spanCorr.offset().top - 273
+                }, 700);
+                previousSpan = spanCorr;
+            }
+    }
+});
 
 });
